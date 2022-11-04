12f90d0318af8038adb6099afab7d9c73e3c0f6b
Shishir Mathur <shishirmns718@gmail.com>
2022-11-05 05:16:34 +0530

    secure API keys location updated

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,10 +1,13 @@
+/// Note: Please update the address here for your local files that contains the API keys.
 abstract class SBAssets {
 
   static const noresultsfound = "assets/images/no_result_found.png";
   static const nointernetconnection = "assets/images/internet_connection_error.png";
+
+  static const apiKeyBaseLocation = "/Users/shishirmathur/Desktop/misc/sb-news-api-keys/";
   
-  static const apiKey1 = "assets/confidential/apiKey1.txt";
-  static const apiKey2 = "assets/confidential/apiKey2.txt";
-  static const apiKey3 = "assets/confidential/apiKey3.txt";
+  static const apiKey1 = "${apiKeyBaseLocation}apiKey1.txt";
+  static const apiKey2 = "${apiKeyBaseLocation}apiKey2.txt";
+  static const apiKey3 = "${apiKeyBaseLocation}apiKey3.txt";
 
 }
